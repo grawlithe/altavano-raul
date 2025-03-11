8cb910fdaedc4217521622b9d42e49336eaab096
grawlithe <raul.altavano@gmail.com>
2025-03-11 17:57:22 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('schedule', function () {
+    return "1";
+})->middleware(['auth', 'verified'])->name('schedule');
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
